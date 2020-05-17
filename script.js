04538fbafed8035be293f2b4e1978799af594602
yashaggarwal07 <ysinghal73@gmail.com>
2020-05-17 23:31:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+var mylist = document.getElementsByTagName("LI");
+
+for (var i = 0; i < mylist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  mylist[i].appendChild(span);
+}
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+function addTodo() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var tODO = document.createTextNode(inputValue);
+  li.appendChild(tODO);
+  if (inputValue === '') {
+   
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+function removeAll(){
+  var lst = document.getElementsByTagName("ul");
+    lst[0].innerHTML = "";
+}
